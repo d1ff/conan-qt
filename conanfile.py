3327e6d1424503e3ebbc243f989a6cde1a85ecc6
ericLemanissier <ericLemanissier@users.noreply.github.com>
2020-04-16 18:24:48 +0200

    don't use -no-framework on macos
    fixes bincrafters/community#1171

--- conanfile.py
+++ conanfile.py
@@ -564,8 +564,6 @@ class QtConan(ConanFile):
         if self.settings.os == "Linux":
             if self.options.GUI:
                 args.append("-system-xcb")
-        elif self.settings.os == "Macos":
-            args += ["-no-framework"]
         elif self.settings.os == "Android":
             args += ["-android-ndk-platform android-%s" % self.settings.os.api_level]
             args += ["-android-abis %s" % {"armv7": "armeabi-v7a",
